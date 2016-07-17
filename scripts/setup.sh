671748be505f0e43b8b18ecd4de0ec72a53fa8a1
Louis-Guillaume Gagnon <louis.guillaume.gagnon@cern.ch>
2016-07-17 11:08:55 +0200

    Add root_graph_utils as submodule

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,2 +1,5 @@
 . /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/user/atlasLocalSetup.sh
 lsetup 'ROOT 6.06.02-x86_64-slc6-gcc49-opt'
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export PYTHONPATH=$(dirname $DIR)/submodules:$PYTHONPATH
