0b6538e9ac7c348c7b188df9bbd91e3fcdb5f059
Louis-Guillaume Gagnon <louis.guillaume.gagnon@cern.ch>
2016-07-21 20:19:34 +0200

    update virtualenv setup

--- scripts/venv-setup.sh
+++ scripts/venv-setup.sh
@@ -1,15 +1,25 @@
-virtualenv --python=$(which python) venv_deepsusy
+# setup and activate the virtualenv, making sure that the first python
+# on PATH is picked-up
+virtualenv --python=$(which python2) venv_deepsusy
 . venv_deepsusy/bin/activate
 
+# in case any of the package have to build from source, make sure that
+# the correct gcc version is picked-up
 export CC=$(which gcc)
 export CXX=$(which g++)
 
-pip install numpy
-pip install scipy
-pip install h5py
-pip install Theano
+# make sure that pip is up-to-date. if not, it will not download
+# wheels and installation time will be very long
+pip install --upgrade pip
+
+# installing keras will pick up the required dependencies such as
+# numpy/scipy, theano and pyyaml
 pip install keras
 
+# h5py is an optional dependency
+pip install h5py
+
+# install ROOT-specific packages if ROOT is installed
 which root >/dev/null
 if [ $? -eq 0 ]
 then
