83c1112bc885346318f1d42a5bb23be4dd516f23
Louis-Guillaume Gagnon <louis.guillaume.gagnon@cern.ch>
2017-06-22 17:25:41 -0400

    Begin working on master hdf5 dataset

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -2,7 +2,10 @@ import collections
 import glob
 import logging
 
+import h5py as h5
+import numpy as np
 import ROOT
+import root_numpy
 
 import utils
 
@@ -77,3 +80,75 @@ def __get_xsec(paths):
         hcut = tf.Get('cut_flow')
         weight += hcut.GetBinContent(2)
     return 1000.0 * xsec / weight
+
+
+def __load(datalist):
+    nrow = np.sum([c.tree.GetEntries() for c in datalist])
+
+    # get all the data into an array
+    for i, data in enumerate(datalist):
+        log.debug('__load dsid=%s', data.dsid)
+        subarray = root_numpy.tree2array(data.tree)
+        if i == 0:
+            array = np.empty(nrow, dtype=subarray.dtype)
+            i0 = 0
+        else:
+            i0 = i1
+        i1 = i0 + subarray.shape[0]
+        array[i0:i1] = subarray
+
+    # Add the gluino and lsp masses for the Gtt samples
+    try:
+        mg, ml = gtt.get_masses(int(data.dsid))
+        array[:, 'I_m_gluino'] = mg
+        array[:, 'I_m_lsp'] = ml
+    except KeyError:
+        pass
+
+    # Shuffle the group
+    np.random.shuffle(array)
+
+    # Split input from metadata
+    inputv = array[:, __get_slice(array, lambda n: n.startswith('I_'))]
+    metadv = array[:, __get_slice(array, lambda n: n.startswith('M_'))]
+
+    # Destructure and downcast input array
+    inputv = structured.view(np.float64).reshape(structured.shape + (-1,)).as_type(np.float32)
+
+    return inputv, metadv
+
+def __get_slice(sarray, selector):
+    names = __get_header(sarray)
+
+    indices = [i for i, name in enumerate(names) if selector(name)]
+
+    LOGGER.debug('indices: %s', str(indices))
+
+    ilow = min(indices)
+    iup = max(indices) + 1
+
+    if not indices == range(ilow, iup):
+        raise RuntimeError('selector yields non-contiguous indices')
+
+    if iup - ilow == 1:
+        return ilow
+    else:
+        return slice(ilow, iup)
+
+
+
+def create_master(datadir, output):
+
+    ddict = lookup(datadir, 'NNinput')
+    with h5.File(utils.unique_path(output), 'x') as outf:
+        for group in ddict:
+            log.info('Adding group: %s', group)
+            outf.create_dataset(
+                name=group,
+                data=__load(ddict[group]),
+                compression='gzip',
+                chunks=True,
+                shuffle=True,
+                track_times=True
+            )
+
