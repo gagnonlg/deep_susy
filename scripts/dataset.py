6f58e320eb069e0799a542f83aaad5b1c737d90b
Louis-Guillaume Gagnon <louis.guillaume.gagnon@cern.ch>
2017-06-23 13:22:59 -0400

    finish master dataset code

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -7,44 +7,46 @@ import numpy as np
 import ROOT
 import root_numpy
 
+import gtt
 import utils
 
 log = logging.getLogger(__name__)
 
 
-def lookup_by_dsid(datadir, dsid, treename, xsec=False):
-    log.debug('datadir: %s', datadir)
-    log.debug('dsid: %s', dsid)
-    log.debug('treename: %s', treename)
-    # first look for subdirectory
-    dset = glob.glob('{}/*.{}.*.root/*.root*'.format(datadir, dsid))
-    if len(dset) == 0:
-        # look for single file
-        dset = glob.glob('{}/{}.*.root'.format(datadir, dsid))
-        if len(dset) == 0:
-            raise RuntimeError('dsid %s not found', dsid)
-    chain = ROOT.TChain(treename)
-    for dpath in dset:
-        chain.Add(dpath)
-    if xsec:
-        return chain, __get_xsec(dset)
-    else:
-        return chain
+def create_master(datadir, output):
 
-
-def __read_config():
-    path = utils.top_directory() + '/datasets.config'
-    key = None
-    ddict = collections.defaultdict(list)
-    with open(path, 'r') as dfile:
-        for line in dfile:
-            if line.startswith('#'):
-                key = line.strip('# \n')
-                log.debug(key)
-            else:
-                ddict[key].append(line.split(' ')[0])
-                log.debug('     %s', ddict[key][-1])
-    return ddict
+    header_saved = False
+    ddict = lookup(datadir, 'NNinput')
+    with h5.File(utils.unique_path(output), 'x') as outf:
+        for group in ddict:
+            log.info('Adding group: %s', group)
+            grp = outf.create_group(name=group)
+            inp, inp_header, meta = __load(ddict[group])
+            grp.create_dataset(
+                name='input',
+                data=inp,
+                compression='gzip',
+                chunks=True,
+                shuffle=True,
+                track_times=True
+            )
+            grp.create_dataset(
+                name='metadata',
+                data=meta,
+                compression='gzip',
+                chunks=True,
+                shuffle=True,
+                track_times=True
+            )
+            if not header_saved:
+                log.debug('HEADER: %s', inp_header)
+                outf.create_dataset(
+                    name='input_header',
+                    shape=(len(inp_header),),
+                    dtype=h5.special_dtype(vlen=bytes),
+                    data=inp_header
+                )
+                header_saved = True
 
 
 def lookup(datadir, treename, xsec=False):
@@ -71,6 +73,26 @@ def lookup(datadir, treename, xsec=False):
     return ddict
 
 
+def lookup_by_dsid(datadir, dsid, treename, xsec=False):
+    log.debug('datadir: %s', datadir)
+    log.debug('dsid: %s', dsid)
+    log.debug('treename: %s', treename)
+    # first look for subdirectory
+    dset = glob.glob('{}/*.{}.*.root/*.root*'.format(datadir, dsid))
+    if len(dset) == 0:
+        # look for single file
+        dset = glob.glob('{}/{}.*.root'.format(datadir, dsid))
+        if len(dset) == 0:
+            raise RuntimeError('dsid %s not found', dsid)
+    chain = ROOT.TChain(treename)
+    for dpath in dset:
+        chain.Add(dpath)
+    if xsec:
+        return chain, __get_xsec(dset)
+    else:
+        return chain
+
+
 def __get_xsec(paths):
     weight = 0
     for path in paths:
@@ -95,60 +117,38 @@ def __load(datalist):
         else:
             i0 = i1
         i1 = i0 + subarray.shape[0]
-        array[i0:i1] = subarray
+        array[i0:i1] = np.copy(subarray)
 
-    # Add the gluino and lsp masses for the Gtt samples
-    try:
-        mg, ml = gtt.get_masses(int(data.dsid))
-        array[:, 'I_m_gluino'] = mg
-        array[:, 'I_m_lsp'] = ml
-    except KeyError:
-        pass
+        # Add the gluino and lsp masses for the Gtt samples
+        try:
+            mg, ml = gtt.get_masses(int(data.dsid))
+            array['I_m_gluino'] = mg
+            array['I_m_lsp'] = ml
+        except KeyError:
+            pass
 
     # Shuffle the group
     np.random.shuffle(array)
 
     # Split input from metadata
-    inputv = array[:, __get_slice(array, lambda n: n.startswith('I_'))]
-    metadv = array[:, __get_slice(array, lambda n: n.startswith('M_'))]
+    names = array.dtype.names
+    iheader = [n for n in names if n.startswith('I_')]
+    inputv = root_numpy.rec2array(array[iheader])
+    metadv = array[[n for n in names if n.startswith('M_')]]
 
-    # Destructure and downcast input array
-    inputv = structured.view(np.float64).reshape(structured.shape + (-1,)).as_type(np.float32)
-
-    return inputv, metadv
-
-def __get_slice(sarray, selector):
-    names = __get_header(sarray)
-
-    indices = [i for i, name in enumerate(names) if selector(name)]
-
-    LOGGER.debug('indices: %s', str(indices))
-
-    ilow = min(indices)
-    iup = max(indices) + 1
-
-    if not indices == range(ilow, iup):
-        raise RuntimeError('selector yields non-contiguous indices')
-
-    if iup - ilow == 1:
-        return ilow
-    else:
-        return slice(ilow, iup)
+    return inputv, iheader, metadv
 
 
-
-def create_master(datadir, output):
-
-    ddict = lookup(datadir, 'NNinput')
-    with h5.File(utils.unique_path(output), 'x') as outf:
-        for group in ddict:
-            log.info('Adding group: %s', group)
-            outf.create_dataset(
-                name=group,
-                data=__load(ddict[group]),
-                compression='gzip',
-                chunks=True,
-                shuffle=True,
-                track_times=True
-            )
-
+def __read_config():
+    path = utils.top_directory() + '/datasets.config'
+    key = None
+    ddict = collections.defaultdict(list)
+    with open(path, 'r') as dfile:
+        for line in dfile:
+            if line.startswith('#'):
+                key = line.strip('# \n')
+                log.debug(key)
+            else:
+                ddict[key].append(line.split(' ')[0])
+                log.debug('     %s', ddict[key][-1])
+    return ddict
