51a2ace08542914a2800a2c52abebdff0d3bf8bb
Louis-Guillaume Gagnon <louis.guillaume.gagnon@gmail.com>
2017-07-24 16:59:25 -0400

    Implement new function to plot signal vs background

---
+++ deep_susy/histograms.py
@@ -0,0 +1,210 @@
+""" functions to draw histograms """
+
+import itertools
+import os
+
+import ROOT
+import root_numpy
+
+from root_graph_utils import atlas_utils
+
+atlas_utils.set_atlas_style()
+ROOT.gROOT.SetBatch()
+ROOT.gStyle.SetPalette(ROOT.kBlueGreenYellow)
+
+WEIGHTS = {}
+
+
+def signal_vs_background(
+        signals,
+        backgrounds,
+        weighted,
+        directory='.',
+        variables='all'):
+    """ Draw signal vs background histograms
+
+    Arguments:
+      -- signals: list of signal h5 groups
+      -- backgrounds: list of background h5 groups
+      -- weighted: if True, weight the samples to their cross-sections
+      -- directory (default: '.'): output directory for the plots
+      -- variables (default: 'all'): list of variables to plot or 'all'
+    """
+
+    if directory != '.':
+        try:
+            os.mkdir(directory)
+        except OSError:
+            pass
+
+    if variables == 'all':
+        variables = [
+            m for m in itertools.chain(
+                [('input', n) for n, _ in
+                 backgrounds[0]['input'].dtype.descr],
+                [('metadata', n) for n, _ in
+                 backgrounds[0]['metadata'].dtype.descr],
+            )
+        ]
+        variables.remove('I_m_gluino')
+        variables.remove('I_m_lsp')
+    else:
+        variables = [
+            ('input' if v.startswith('I_') else 'metadata', v)
+            for v in variables
+        ]
+
+    for dset, var in variables:
+        leg = ROOT.TLegend(0.75, 0.5, 0.95, 0.9)
+        bstack, bint = _stack(backgrounds, dset, var, weighted, leg)
+        shists = _hists(signals, dset, var, weighted, leg, bint)
+        _draw(bstack, shists, var, weighted, directory, leg)
+
+
+def _get_weights(dset):
+    if dset.name not in WEIGHTS:
+        WEIGHTS[dset.name] = dset['metadata'].value['M_weight']
+    return WEIGHTS[dset.name]
+
+
+def _draw(bstack, shists, var, weighted, directory, legend):
+    # pylint: disable=too-many-arguments
+    cnv = ROOT.TCanvas("c", "", 0, 0, 800, 600)
+    cnv.SetLogy()
+    bstack.SetMaximum(bstack.GetMaximum() * 5)
+    unit = _units(var)
+    bstack.SetTitle(
+        ';{} [{}];{}'.format(
+            var,
+            unit,
+            '{} / {} {}'.format(
+                'Cross section [fb]' if weighted else 'Events',
+                shists[0].GetBinWidth(1),
+                unit
+            )
+        )
+    )
+    bstack.Draw("hist")
+    bstack.GetXaxis().SetLabelSize(0.04)
+    for hist in shists:
+        hist.Draw("hist same")
+
+    legend.SetBorderSize(0)
+    legend.Draw()
+    atlas_utils.atlas_label(0.2, 0.88)
+    txt = ROOT.TLatex()
+    txt.SetNDC()
+    txt.DrawText(0.325, 0.88, 'Internal simulation')
+
+    cnv.SaveAs(
+        "{}/h_{}{}.pdf".format(
+            directory if directory is not None else '.',
+            var,
+            '_weighted' if weighted else ''
+        )
+    )
+
+
+def _stack(groups, dset, var, weighted, legend):
+    stk = ROOT.THStack('stk_{}_{}'.format(var, weighted), '')
+    icols = [0, 90, 125, 200]
+    stkint = 0
+    for i, grp in enumerate(groups):
+        data = grp[dset].value[var]
+        if weighted:
+            weights = _get_weights(grp)
+        else:
+            weights = None
+        name = grp.name.split('/')[-1]
+        hist = ROOT.TH1D(
+            'h_{}_{}_{}'.format(name, var, weighted),
+            '',
+            *_get_range(var)
+        )
+        col = ROOT.gStyle.GetColorPalette(icols[i])
+        hist.SetLineWidth(0)
+        hist.SetFillColor(col)
+        legend.AddEntry(hist, name, 'F')
+        root_numpy.fill_hist(hist, data, weights)
+        stkint += hist.Integral()
+        stk.Add(hist)
+    return stk, stkint
+
+
+def _hists(groups, dset, var, weighted, legend, stkint):
+    # pylint: disable=too-many-arguments
+    lst = []
+    cols = [ROOT.kGray, ROOT.kRed, ROOT.kYellow]
+    for i, grp in enumerate(groups):
+        data = grp[dset].value[var]
+        if weighted:
+            weights = _get_weights(grp)
+        else:
+            weights = None
+        name = grp.name.split('/')[-1]
+        hist = ROOT.TH1D(
+            'h_{}_{}_{}'.format(name, var, weighted),
+            '',
+            *_get_range(var)
+        )
+        hist.SetLineColor(cols[i])
+        legend.AddEntry(hist, name, 'L')
+        root_numpy.fill_hist(hist, data, weights)
+        hist.Scale(stkint / hist.Integral())
+        lst.append(hist)
+    return lst
+
+
+def _get_range(var):
+    #  pylint: disable=too-many-return-statements,too-many-branches
+    if ('_px' in var) or ('_py' in var):
+        if 'lepton' in var:
+            return 100, -1000, 1000
+        return 100, -2500, 2500
+    if '_pz' in var:
+        if 'lepton' in var:
+            return 100, -1500, 1500
+        return 100, -3000, 3000
+    if '_e_' in var:
+        if 'lepton' in var:
+            return 100, 0, 2000
+        return 100, 000, 5000
+    if '_eta_' in var:
+        return 100, -3, 6
+    if '_phi' in var:
+        return 100, -3.5, 6.5
+    if '_m_' in var:
+        if 'lepton' in var:
+            return 100, -0.5, 0.5
+        return 100, 0, 300
+    if '_isb_' in var:
+        return 2, 0, 2
+    if 'met_mag' in var:
+        return 100, 200, 2000
+    if 'weight' in var:
+        return 100, 0, 0.05
+    if '_meff' in var:
+        return 100, 0, 8000
+    if '_mt' in var:
+        return 100, 0, 2000
+    if '_mjsum' in var:
+        return 100, 0, 1500
+    if '_nb77' in var:
+        return 8, 2, 10
+    if '_nlepton' in var:
+        return 8, 0, 8
+    if '_njet30' in var:
+        return 16, 4, 20
+    if '_dphimin4j' in var:
+        return 100, 0, 4
+
+
+def _units(var):
+    if '_px' in var or '_py' in var or '_pz' or '_e_' in var:
+        return 'GeV'
+    if '_m_gluino_' in var or '_m_lsp' in var:
+        return 'GeV'
+    if var in ['M_meff', 'M_mt', 'M_mtb', 'M_mjsum', 'M_met']:
+        return 'GeV'
+    else:
+        return ''
--- scripts/draw_signal_vs_bkg.py
+++ scripts/draw_signal_vs_bkg.py
@@ -1,185 +1,46 @@
 import argparse
-import collections
-import glob
-import logging
-import os
 
-import ROOT
+import h5py as h5
 
-import dataset
-import gtt
-from root_graph_utils import atlas_utils
-import utils
+from deep_susy import histograms, utils
 
-ROOT.gROOT.SetBatch(True)
-atlas_utils.set_atlas_style()
-
-log = logging.getLogger(__name__)
-
-bkeys = ['WZjets', 'topEW', 'Diboson', 'ttbar', 'singletop']
+BKGND_KEYS = ['WZjets', 'topEW', 'singletop', 'ttbar']
+SIGNAL_KEYS = ['Gtt_2100_5000_1', 'Gtt_2100_5000_800', 'Gtt_2100_5000_1600']
 
 
-def get_range(var):
-    if ('_px_' in var) or ('_py_' in var):
-        return 100, -2500, 2500
-    if  '_pz_' in var:
-        return 100, -3000, 3000
-    if '_e_' in var:
-        return 100, 000, 5000
-    if '_eta_' in var:
-        return 100, -3, 6
-    if '_phi' in var:
-        return 100, -3.5, 6.5
-    if '_m_' in var:
-        if 'lepton' in var:
-            return 100, -0.5, 0.5
-        return 100, 0, 300
-    if '_isb_' in var:
-        return 2, 0, 2
-    if 'met_mag' in var:
-        return 100, 200, 2000
-    if 'weight' in var:
-        return 100, 0, 0.05
-    if '_meff' in var:
-        return 100, 0, 8000
-    if '_mt' in var:
-        return 100, 0, 2000
-    if '_mjsum' in var:
-        return 100, 0, 1500
-    if '_nb77' in var:
-        return 8, 2, 10
-    if '_nlepton' in var:
-        return 8, 0, 8
-    if '_njet30' in var:
-        return 16, 4, 20
-    if '_dphimin4j' in var:
-        return 100, 0, 4
-
-def get_hist(ddict, key, var, weighted):
-    hname = 'h_{}_{}'.format(var, key)
-    weight = 'M_weight' if weighted else '1.0'
-    if ROOT.gDirectory.FindObject(hname) == None:
-        log.debug('var: %s', var)
-        nbin,vmin,vmax = get_range(var)
-        rg = '({},{},{})'.format(nbin,vmin,vmax)
-        varexp = '{}>>{}' + rg
-    else:
-        varexp = '{}>>+{}'
-    for data in ddict[key]:
-        data.tree.Draw(varexp.format(var, hname), weight)
-    return ROOT.gDirectory.Get(hname)
-
-def get_gtt_hist(ddict, mg, ml, var, weighted, stkint):
-    dsid = str(gtt.get_dsid(mg, ml))
-    weight = 'M_weight' if weighted else '1.0'
-    for data in ddict['Gtt']:
-        _dsid = data.dsid
-        tree = data.tree
-        if dsid == _dsid:
-            h_name = 'h_{}_{}'.format(var, dsid)
-            nbin,vmin,vmax = get_range(var)
-            rg = '({},{},{})'.format(nbin,vmin,vmax)
-            varexp = '{}>>{}' + rg
-            varexp = varexp.format(var, h_name)
-            tree.Draw(varexp, weight)
-            h = ROOT.gDirectory.Get(h_name)
-            #if weighted:
-            h.Scale(stkint / h.Integral())
-            return h
-
-def get_color(idist, ndist):
-    #return ROOT.gStyle.GetcolorPalette(idist * 255 / ndist)
-    if idist == 0:
-        return ROOT.kBlack
-    else:
-        return ROOT.gStyle.GetColorPalette(idist * 255 / (ndist - 1))
-
-def get_stack(ddict, var, weighted):
-    log.debug('var: %s', var)
-    ROOT.gStyle.SetPalette(ROOT.kBlueGreenYellow)
-    stk = ROOT.THStack("stk_{}".format(var), "")
-    stkint = 0
-    leg = ROOT.TLegend(0.7, 0.5, 0.9, 0.9)
-    hlist = []
-    stkint = 0
-    for i, key in enumerate(bkeys):
-        h = get_hist(ddict, key, var, weighted)
-        h.SetFillColor(get_color(i, len(ddict)))
-        h.SetLineWidth(0)
-        hlist.append(h)
-        stk.Add(h)
-        stkint += h.Integral()
-
-    for h, key in zip(hlist, bkeys)[::-1]:
-        leg.AddEntry(h, key, "F")
-
-    hg = []
-    hg.append(get_gtt_hist(ddict, 2100, 1, var, weighted, stkint))
-    hg[-1].SetLineColor(ROOT.kGray)
-    leg.AddEntry(hg[-1], 'mg=2100, ml=1', 'L')
-    hg.append(get_gtt_hist(ddict, 2100, 800, var, weighted, stkint))
-    hg[-1].SetLineColor(ROOT.kRed)
-    leg.AddEntry(hg[-1], 'mg=2100, ml=800', 'L')
-    hg.append(get_gtt_hist(ddict, 2100, 1600, var, weighted, stkint))
-    hg[-1].SetLineColor(ROOT.kYellow)
-    leg.AddEntry(hg[-1], 'mg=2100, ml=1600', 'L')
-
-    leg.SetBorderSize(0)
-    return stk, leg, hg
-
-def draw_var(ddict, var, weighted):
-    c = ROOT.TCanvas("c", "", 0, 0, 800, 600)
-    c.SetLogy()
-    stk,leg,hg = get_stack(ddict, var, weighted)
-    stk.SetMaximum(stk.GetMaximum() * 2)
-
-    stk.SetTitle(';{};{}'.format(var, 'Cross section [fb]' if weighted else 'Events'))
-
-    stk.Draw("hist")
-    for h in hg:
-        h.Draw("hist same")
-
-    leg.Draw()
-    atlas_utils.atlas_label(0.2, 0.88)
-    txt = ROOT.TLatex()
-    txt.SetNDC()
-    txt.DrawText(0.325, 0.88, 'Internal simulation')
-
-    c.SaveAs("h_{}{}.pdf".format(var, '_weighted' if weighted else ''))
-
-
-def get_var_list(datasets):
-    tree = datasets.itervalues().next()[0].tree
-    skiplist = [
-        'I_m_gluino',
-        'I_m_lsp',
-        'M_event_number',
-        'M_run_number',
-        'M_met',
-        'M_dsid',
-        'L_target'
-    ]
-    for brn in [br.GetName() for br in tree.GetListOfBranches()]:
-        if brn not in skiplist:
-            yield brn
-
-
-def get_args():
+def _get_args():
     args = argparse.ArgumentParser()
-    args.add_argument('--datadir', required=True)
+    args.add_argument('masterh5')
+    args.add_argument('--unweighted', action='store_true')
+    args.add_argument('--output', default='.')
+    args.add_argument('--variables', nargs='+')
+    args.add_argument(
+        '--ttbar',
+        choices=['MGPy8', 'PhHpp', 'PhPy6'],
+        default='PhPy6'
+    )
     return args.parse_args()
 
-def main():
 
-    args = get_args()
+def _main():
+    args = _get_args()
+    h5file = h5.File(args.masterh5)
 
-    datasets = dataset.lookup(args.datadir, 'NNinput')
+    if args.ttbar != 'PhPy6':
+        BKGND_KEYS.remove('ttbar')
+        BKGND_KEYS.append(args.ttbar + '_ttbar')
 
-    for var in get_var_list(datasets):
-        # print '==> ' + var
-        draw_var(datasets, var, weighted=True)
-        draw_var(datasets, var, weighted=False)
+    s_dsets = [h5file['signal/' + k] for k in SIGNAL_KEYS]
+    b_dsets = [h5file['background/' + k] for k in BKGND_KEYS]
+
+    histograms.signal_vs_background(
+        signals=s_dsets,
+        backgrounds=b_dsets,
+        weighted=not args.unweighted,
+        variables='all' if args.variables is None else args.variables,
+        directory=args.output
+    )
 
 
 if __name__ == '__main__':
-    utils.main(main, 'signal_vs_bkg')
+    utils.main(_main, 'draw_signal_vs_bkg')
