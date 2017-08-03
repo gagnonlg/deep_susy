8c06759771a49962041eb92b1e1e748434c42457
Louis-Guillaume Gagnon <louis.guillaume.gagnon@gmail.com>
2017-08-03 09:28:50 -0400

    Add a dummy test model

---
+++ models/test_model.py
@@ -0,0 +1,23 @@
+""" Implementation of model from 1402.4735 """
+
+import math
+
+import keras
+import numpy as np
+
+
+def build_model(model, x_dset, y_dset, *args, **kwargs):
+    """ build the model """
+    # pylint: disable=unused-argument
+    input_node = keras.layers.Input((x_dset.shape[1],))
+    k_model = keras.layers.Dense(10, activation='sigmoid')(input_node)
+    output_node = keras.layers.Dense(y_dset.shape[1], activation='softmax')(input_node)
+    k_model = keras.models.Model(inputs=input_node, outputs=output_node)
+
+    model['name'] = 'TEST'
+    model['keras_model'] = k_model
+    model['optimizer'] = keras.optimizers.SGD()
+    model['loss'] = 'categorical_crossentropy'
+    model['callbacks'] = []
+    model['batch_size'] = 32
+    model['max_epochs'] = 10
