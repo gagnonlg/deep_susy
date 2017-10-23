924b9f691cae996afc5d6efd87ae2be5f0258b83
Louis-Guillaume Gagnon <louis.guillaume.gagnon@gmail.com>
2017-10-23 11:55:57 -0400

    submit_h5: fix the virtualenv path

--- scripts/current_dataset/submit_h5.sh
+++ scripts/current_dataset/submit_h5.sh
@@ -7,6 +7,6 @@ cat <<EOF | qsub -d $PWD -N $(basename $DATADIR) -joe
 mkdir \${PBS_JOBID}_\${PBS_JOBNAME}
 cd \${PBS_JOBID}_\${PBS_JOBNAME}
 source $CODEDIR/scripts/setup.sh
-source $PWD/bin/activate
+source $PWD/venv/bin/activate
 python2 $CODEDIR/scripts/current_dataset/create_h5.py $DATADIR
 EOF
